7260d589deadc23156ca2c87e792f4c233b6595e
Fariz Ramadhani <farizramadhani96@gmail.com>
2022-06-20 17:52:23 +0700

    added simple-screen app rexfariz

---
+++ simple-screen/simple-screen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  simple-screen
+//
+//  Created by Fariz Ramadhani on 20/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
